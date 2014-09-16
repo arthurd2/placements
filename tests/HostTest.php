b2a12f02c16e5a8b450709c188acde352079f020
Guilherme Geronimo <guilherme.geronimo@ufsc.br>
2014-09-15 23:49:17 -0300

    TEST: Arrumando constantes

--- tests/HostTest.php
+++ tests/HostTest.php
@@ -1,17 +1,7 @@
 <?php
 require_once "src/model/VM.php";
 require_once "src/model/Host.php";
-//require_once "tests/constants.php";
-
-define('VM_NAME', 'VM_1');
-define('VM_NAME2', 'VM_2');
-define('D_RAM', 'RAM');
-define('D_RAM_V', 512);
-define('D_RAM_V2', 1024);
-define('D_RAM_V3', 2048);
-define('D_PROC', 'Processors');
-define('D_PROC_V', 4);
-define('D_PROC_V2', 8);
+require_once "tests/constants.php";
 
 
 class PacoteTest extends PHPUnit_Framework_TestCase
--- tests/constants.php
+++ tests/constants.php
@@ -5,4 +5,7 @@ define('VM_NAME2', 'VM_2');
 define('D_RAM', 'RAM');
 define('D_RAM_V', 512);
 define('D_RAM_V2', 1024);
+define('D_RAM_V3', 2048);
 define('D_PROC', 'Processors');
+define('D_PROC_V', 4);
+define('D_PROC_V2', 8);
