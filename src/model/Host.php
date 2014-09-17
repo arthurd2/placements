ef8d70da0676e33cdf2da73fab43d8e53ae52d63
Guilherme Geronimo <guilherme.geronimo@ufsc.br>
2014-09-17 01:37:06 -0300

    SONAR: Zerando

--- src/model/Host.php
+++ src/model/Host.php
@@ -1,32 +1,33 @@
 <?php
-class Host {
+class Host
+{
     private $freeDimensions;
     private $dimensions;
     private $vms = array();
     private $name;
-
+    
     function __construct($name, array $dimensions) {
         $this->name = $name;
         $this->dimensions = $dimensions;
         $this->freeDimensions = $dimensions;
     }
-
+    
     public function getVMs() {
         return $this->vms;
     }
-
+    
     public function getName() {
         return $this->name;
     }
-
+    
     public function getFreeDimensions() {
         return $this->freeDimensions;
     }
-
+    
     public function getDimensions() {
         return $this->dimensions;
     }
-
+    
     public function fitVM(VM $vm) {
         foreach ($this->freeDimensions as $dimension => $freeSpace) {
             if ($freeSpace < $vm->getDimension($dimension)) {
@@ -35,7 +36,7 @@ class Host {
         }
         return true;
     }
-
+    
     public function storeVM($vm) {
         if ($this->fitVM($vm)) {
             try {
@@ -49,7 +50,7 @@ class Host {
         }
         return false;
     }
-
+    
     private function substractVMDimesions($vm) {
         foreach ($this->freeDimensions as $dimension => $freeSpace) {
             $substract = $vm->getDimension($dimension);
--- src/model/rule.php
+++ src/model/rule.php
@@ -1,27 +1,26 @@
 <?php
-class Rule {
+class Rule
+{
     protected $matrix;
-
+    
     function __construct($matrix = null) {
         $this->matrix = $matrix;
     }
-
+    
     function setMatrix($matrix) {
-
-        if (!is_null($this->matrix)) 
-        	return false;
+        
+        if (!is_null($this->matrix)) return false;
         $this->matrix = $matrix;
         return true;
     }
-
+    
     function isPermited($host, $vm) {
-
-        if (is_null($this->matrix))
-        	throw new Exception("TestingWithoutMatrix", 1);
+        
+        if (is_null($this->matrix)) throw new Exception("TestingWithoutMatrix", 1);
         
         return isset($this->matrix[$host][$vm]) ? $this->matrix[$host][$vm] : false;
     }
-
+    
     static function mergeRules(&$hosts, &$vms, &$rules) {
         $mergedRules = array();
         foreach ($hosts as $host) {
@@ -31,7 +30,7 @@ class Rule {
         }
         return new Rule($mergedRules);
     }
-
+    
     static private function mergeRule(&$host, &$vm, &$rules) {
         foreach ($rules as $rule) {
             if (!$rule->isPermited($host->getName(), $vm->getName())) {
