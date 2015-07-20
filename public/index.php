b5a7be0998f27180f251304fb5aec7157044c152
guilherme.geronimo <guilherme.geronimo@gmail.com>
2015-07-21 01:22:07 +0200

    ...

--- public/index.php
+++ public/index.php
@@ -11,7 +11,8 @@ $filemane = 'Scenarios_APR(0.75)_VMs(4,5,6,7,8,9)_PMs(3).2';
 
 $file = file($path . $filemane . '.json');
 $scenarios = json_decode($file[0], true);
-$scenario = array_shift($scenarios);
+$scenarios = array_reverse($scenarios);
+$scenario = array_pop($scenarios);
 
 $header = Scenario::toGoogleTableHeader($scenario);
 $lines = Scenario::toGoogleTableLines($scenario);
