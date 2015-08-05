324158b6d675f9d875dfb8bf047247dc299afaab
guilherme.geronimo <guilherme.geronimo@gmail.com>
2015-08-05 14:04:14 +0200

    Adding ENVVAR tests

---
+++ tests/envTest.php
@@ -0,0 +1,15 @@
+<?php
+class envTests extends PHPUnit_Framework_TestCase {
+
+	function testEnvVars(){
+		$neededEnvVars[] = 'VMHOST';
+		$neededEnvVars[] = 'VMUSER';
+		$neededEnvVars[] = 'VMPASS';
+
+		foreach ($neededEnvVars as $value) {
+			$this->assertFalse(empty(getenv($value)),"Env Var '$value' is not set.");
+		}
+		
+	}
+
+}
